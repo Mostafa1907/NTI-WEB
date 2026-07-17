89229bd248c5f34b6e3ccb5a136c22b0b548badc
Mostafa1907 <mostafahossam2006@gmail.com>
2026-07-18 02:14:19 +0300

    Add files via upload

---
+++ LeetCode session 1/LeetCode 1.js
@@ -0,0 +1,44 @@
+function isValid(s) {
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (s[i] == "(" || s[i] == "[" || s[i] == "{"){
+            stack.push(s[i]);
+            } 
+            else 
+            {
+
+            if (stack.length == 0)
+            {
+                return false;
+            }
+
+            let last = stack.pop();
+
+            if (s[i] == ")" && last != "(")
+            {
+                return false;
+            }
+
+            if (s[i] == "]" && last != "[") 
+            {
+                return false;
+            }
+
+            if (s[i] == "}" && last != "{")\
+            {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length == 0) 
+    {
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+}
